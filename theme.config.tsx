ef08a855f76b6231c67097d140b5ad0c7073672d
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-11-24 15:59:00 +0000

    restore nextra docs stylesheet

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import "nextra-theme-docs/style.css";
 
 const config: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/rezrah/nextocat",
