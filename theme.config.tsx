f627ed47e8c04c449e410f896894c08630cde9db
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-11-24 15:38:01 +0000

    remove unnecessary stylesheet

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
-import "nextra-theme-docs-custom/style.css";
+
 
 const config: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/rezrah/nextocat",
