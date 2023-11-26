a7b812d4e866eb837adc6bcf531e7c6fdee4ed47
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-11-26 10:02:20 +0000

    increase page container to full width

--- @primer-nextocat/header/Header.tsx
+++ @primer-nextocat/header/Header.tsx
@@ -1,63 +1,63 @@
-import React from "react";
-import { SubdomainNavBar } from "@primer/react-brand";
-import { PageMapItem, MdxFile } from "nextra";
-import { useRouter } from "next/router";
+import React from 'react'
+import {SubdomainNavBar} from '@primer/react-brand'
+import {PageMapItem, MdxFile} from 'nextra'
+import {useRouter} from 'next/router'
 
 type HeaderProps = {
-  pageMap: PageMapItem[];
-};
+  pageMap: PageMapItem[]
+}
 
-export function Header({ pageMap }: HeaderProps) {
-  const router = useRouter();
-  const basePath = router.basePath;
-  const inputRef = React.useRef(null);
-  const [searchResults, setSearchResults] = React.useState([]);
-  const [searchTerm, setSearchTerm] = React.useState("");
+export function Header({pageMap}: HeaderProps) {
+  const router = useRouter()
+  const basePath = router.basePath
+  const inputRef = React.useRef(null)
+  const [searchResults, setSearchResults] = React.useState([])
+  const [searchTerm, setSearchTerm] = React.useState('')
   const searchData = pageMap
-    .map((item) => {
-      if (item.kind === "Folder") {
-        return item.children.filter((child) => child.kind === "MdxPage");
+    .map(item => {
+      if (item.kind === 'Folder') {
+        return item.children.filter(child => child.kind === 'MdxPage')
       }
-      if (item.kind === "MdxPage") {
-        return item;
+      if (item.kind === 'MdxPage') {
+        return item
       }
     })
     .flat()
     .filter(Boolean)
-    .map(({ name, route }: MdxFile) => {
+    .map(({name, route}: MdxFile) => {
       const result = {
         title: name,
         url: route,
-      };
-      return result;
-    });
+      }
+      return result
+    })
 
   const handleChange = () => {
-    if (!inputRef.current) return;
+    if (!inputRef.current) return
     if (inputRef.current.value.length === 0) {
-      setSearchResults(undefined);
-      return;
+      setSearchResults(undefined)
+      return
     }
     if (inputRef.current.value.length > 2) {
-      setTimeout(() => setSearchResults(searchData), 1000);
-      setSearchTerm(inputRef.current.value);
-      return;
+      setTimeout(() => setSearchResults(searchData), 1000)
+      setSearchTerm(inputRef.current.value)
+      return
     }
-  };
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!inputRef.current) return;
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!inputRef.current) return
     if (!inputRef.current.value) {
-      alert(`Enter a value and try again.`);
-      return;
+      alert(`Enter a value and try again.`)
+      return
     }
 
-    alert(`Name: ${inputRef.current.value}`);
-  };
+    alert(`Name: ${inputRef.current.value}`)
+  }
 
   return (
-    <SubdomainNavBar title="Brand toolkit" titleHref={basePath || "/"}>
+    <SubdomainNavBar title="Brand toolkit" titleHref={basePath || '/'} fullWidth>
       <SubdomainNavBar.Search
         ref={inputRef}
         searchTerm={searchTerm}
@@ -65,9 +65,7 @@ export function Header({ pageMap }: HeaderProps) {
         onChange={handleChange}
         searchResults={searchResults}
       />
-      <SubdomainNavBar.SecondaryAction href="#">
-        Secondary CTA
-      </SubdomainNavBar.SecondaryAction>
+      <SubdomainNavBar.SecondaryAction href="#">Secondary CTA</SubdomainNavBar.SecondaryAction>
     </SubdomainNavBar>
-  );
+  )
 }
--- @primer-nextocat/index.tsx
+++ @primer-nextocat/index.tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import React, { useEffect } from "react";
-import type { NextraThemeLayoutProps } from "nextra";
-import { Sidebar } from "./sidebar/Sidebar";
+import Head from 'next/head'
+import React, {useEffect} from 'react'
+import type {NextraThemeLayoutProps} from 'nextra'
+import {Sidebar} from './sidebar/Sidebar'
 import {
   MinimalFooter,
   ThemeProvider as BrandThemeProvider,
@@ -14,36 +14,29 @@ import {
   Stack,
   Text,
   useTheme,
-} from "@primer/react-brand";
-import {
-  Box as PRCBox,
-  ActionList,
-  ActionMenu,
-  BaseStyles,
-  PageLayout,
-  ThemeProvider,
-} from "@primer/react";
+} from '@primer/react-brand'
+import {Box as PRCBox, ActionList, ActionMenu, BaseStyles, PageLayout, ThemeProvider} from '@primer/react'
 
-import "@primer/react-brand/lib/css/main.css";
-import "@primer/react-brand/fonts/fonts.css";
+import '@primer/react-brand/lib/css/main.css'
+import '@primer/react-brand/fonts/fonts.css'
 
-import bodyStyles from "./css/prose.module.css";
-import { Header } from "./header/Header";
+import bodyStyles from './css/prose.module.css'
+import {Header} from './header/Header'
 
-import themeConfig from "../theme.config";
-import { TableOfContents } from "./components/table-of-contents/TableOfContents";
-import { MoonIcon, SunIcon, SyncIcon } from "@primer/octicons-react";
+import themeConfig from '../theme.config'
+import {TableOfContents} from './components/table-of-contents/TableOfContents'
+import {MoonIcon, SunIcon, SyncIcon} from '@primer/octicons-react'
 
-export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
-  const [colorMode, setColorMode] = React.useState<"light" | "dark">("light");
+export default function Layout({children, pageOpts}: NextraThemeLayoutProps) {
+  const [colorMode, setColorMode] = React.useState<'light' | 'dark'>('light')
 
-  const { title, frontMatter, headings, filePath, pageMap, route } = pageOpts;
+  const {title, frontMatter, headings, filePath, pageMap, route} = pageOpts
 
-  const isHomePage = route === "/";
+  const isHomePage = route === '/'
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-color-mode", colorMode);
-  }, [colorMode]);
+    document.documentElement.setAttribute('data-color-mode', colorMode)
+  }, [colorMode])
 
   return (
     <BrandThemeProvider dir="ltr" colorMode={colorMode}>
@@ -54,30 +47,20 @@ export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
             <title>{title}</title>
             <meta name="og:image" content={frontMatter.image} />
           </Head>
-          <AnimationProvider
-            runOnce
-            visibilityOptions={1}
-            autoStaggerChildren={false}
-          >
+          <AnimationProvider runOnce visibilityOptions={1} autoStaggerChildren={false}>
             <Animate animate="fade-in">
-              <PageLayout
-                containerWidth="xlarge"
-                padding="none"
-                sx={{ pt: 76 }}
-              >
+              <PageLayout containerWidth="full" padding="none" sx={{pt: 76}}>
                 <PageLayout.Header>
                   <Header pageMap={pageMap} />
                 </PageLayout.Header>
                 <PageLayout.Content padding="normal">
                   <Grid>
-                    <Grid.Column span={!isHomePage && { large: 9 }}>
-                      <article className={route != "/" ? bodyStyles.Prose : ""}>
+                    <Grid.Column span={!isHomePage && {large: 9}}>
+                      <article className={route != '/' ? bodyStyles.Prose : ''}>
                         {children}
                         <Box marginBlockStart={64}>
                           <p>
-                            <InlineLink
-                              href={`${themeConfig.docsRepositoryBase}/blob/main/${filePath}`}
-                            >
+                            <InlineLink href={`${themeConfig.docsRepositoryBase}/blob/main/${filePath}`}>
                               Edit this page
                             </InlineLink>
                           </p>
@@ -85,7 +68,7 @@ export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
                       </article>
                     </Grid.Column>
                     {!isHomePage && (
-                      <Grid.Column span={{ large: 3 }}>
+                      <Grid.Column span={{large: 3}}>
                         <TableOfContents headings={headings} />
                       </Grid.Column>
                     )}
@@ -107,7 +90,7 @@ export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
                   <Sidebar routes={pageMap} />
                   <PRCBox
                     sx={{
-                      borderTop: "1px solid var(--brand-color-border-muted)",
+                      borderTop: '1px solid var(--brand-color-border-muted)',
                       mt: 4,
                       pt: 4,
                     }}
@@ -117,27 +100,17 @@ export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
                         variant="invisible"
                         alignContent="start"
                         block
-                        leadingVisual={
-                          colorMode === "light" ? SunIcon : MoonIcon
-                        }
+                        leadingVisual={colorMode === 'light' ? SunIcon : MoonIcon}
                       >
-                        {colorMode === "light" && "Light"}
-                        {colorMode === "dark" && "Dark"}
+                        {colorMode === 'light' && 'Light'}
+                        {colorMode === 'dark' && 'Dark'}
                       </ActionMenu.Button>
 
                       <ActionMenu.Overlay>
                         <ActionList>
-                          <ActionList.Item
-                            onSelect={() => setColorMode("light")}
-                          >
-                            Light
-                          </ActionList.Item>
+                          <ActionList.Item onSelect={() => setColorMode('light')}>Light</ActionList.Item>
                           <ActionList.Divider />
-                          <ActionList.Item
-                            onSelect={() => setColorMode("dark")}
-                          >
-                            Dark
-                          </ActionList.Item>
+                          <ActionList.Item onSelect={() => setColorMode('dark')}>Dark</ActionList.Item>
                         </ActionList>
                       </ActionMenu.Overlay>
                     </ActionMenu>
@@ -152,5 +125,5 @@ export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
         </BaseStyles>
       </ThemeProvider>
     </BrandThemeProvider>
-  );
+  )
 }
