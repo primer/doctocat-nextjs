e016e1670d31c75e78e15941beae71ba4e343d24
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-11-28 11:13:45 +0000

    switch to built in tree walker

--- @primer-nextocat/index.tsx
+++ @primer-nextocat/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import React, {useEffect} from 'react'
+import React, {useEffect, useMemo} from 'react'
 import type {Folder, MdxFile, Meta, NextraThemeLayoutProps, PageMapItem} from 'nextra'
+import {useFSRoute, useMounted} from 'nextra/hooks'
+
 import {Sidebar} from './components/layout/sidebar/Sidebar'
 import {
   MinimalFooter,
@@ -24,36 +26,8 @@ import bodyStyles from './css/prose.module.css'
 import {Header} from './components/layout/header/Header'
 import {TableOfContents} from './components/layout/table-of-contents/TableOfContents'
 import themeConfig from '../theme.config'
+import {normalizePages} from 'nextra/normalize-pages'
 import {useRouter} from 'next/router'
-import Link from 'next/link'
-
-function findParent(array: PageMapItem[], filePath: string): Folder | null {
-  const isIndexFilePath = filePath.endsWith('index')
-
-  for (const item of array) {
-    if (item.kind === 'Folder') {
-      if (item.children) {
-        if (isIndexFilePath) {
-          const indexParentFolder = item.children.find(
-            child => child.kind === 'Folder' && child.route === filePath.replace(/\/index$/, ''),
-          )
-          return indexParentFolder as Folder
-        }
-
-        if (item.children.some(child => child.kind === 'MdxPage' && child.route === filePath)) {
-          return item
-        }
-
-        const found = findParent(item.children, filePath)
-        if (found) {
-          return found
-        }
-      }
-    }
-  }
-
-  return null
-}
 
 /**
  * Catch-all layout component
@@ -62,14 +36,34 @@ function findParent(array: PageMapItem[], filePath: string): Folder | null {
  * and export a component with the same name as the layout file
  */
 export default function Layout({children, pageOpts}: NextraThemeLayoutProps) {
-  const [colorMode, setColorMode] = React.useState<'light' | 'dark'>('light')
-
   const {title, frontMatter, headings, filePath, pageMap, route} = pageOpts
+  const {locale = 'en-US', defaultLocale} = useRouter()
+  const fsPath = useFSRoute()
+  const [colorMode, setColorMode] = React.useState<'light' | 'dark'>('light')
+  const {
+    activeType,
+    activeIndex,
+    activeThemeContext,
+    activePath,
+    topLevelNavbarItems,
+    docsDirectories,
+    flatDirectories,
+    flatDocsDirectories,
+    directories,
+  } = useMemo(
+    () =>
+      normalizePages({
+        list: pageMap,
+        locale,
+        defaultLocale,
+        route: fsPath,
+      }),
+    [pageMap, locale, defaultLocale, fsPath],
+  )
 
   const isHomePage = route === '/'
-  const cleanPath = `/${filePath.replace(/^pages\//, '').replace(/\.mdx$/, '')}`
-  const data = !isHomePage && findParent(pageMap, cleanPath)
 
+  const data = !isHomePage && activePath[activePath.length - 2]
   const filteredTabData: MdxFile[] =
     data?.kind === 'Folder' ? (data.children.filter(child => child.kind === 'MdxPage') as MdxFile[]) : []
 
@@ -96,14 +90,14 @@ export default function Layout({children, pageOpts}: NextraThemeLayoutProps) {
                   <Grid>
                     <Grid.Column span={!isHomePage && {large: 9}}>
                       <Stack direction="vertical" padding="none" gap="spacious">
-                        {frontMatter.title && (
+                        {!isHomePage && frontMatter.title && (
                           <Box marginBlockEnd={24}>
                             <Heading as="h1" size="3">
                               {frontMatter.title}
                             </Heading>
                           </Box>
                         )}
-                        {Boolean(frontMatter['show-tabs']) && <UnderlineNav tabData={filteredTabData} />}
+                        {!isHomePage && Boolean(frontMatter['show-tabs']) && <UnderlineNav tabData={filteredTabData} />}
                         <article className={route != '/' ? bodyStyles.Prose : ''}>{children}</article>
                         <Box marginBlockStart={64}>
                           <Stack direction="horizontal" padding="none" alignItems="center" gap={8}>
