1d7f41f50bb74c3360a6fedf576020fb17f54c2b
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2025-02-10 11:54:16 +0000

    fixing bugs found during upgrade and adding migration guide

--- packages/theme/components/content/caption/Caption.tsx
+++ packages/theme/components/content/caption/Caption.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {PropsWithChildren} from 'react'
 import {Text} from '@primer/react'
 
-export function Caption(props) {
+export function Caption(props: PropsWithChildren) {
   return <Text as="p" {...props} sx={{mt: 2, mb: 3, fontSize: 1, color: 'var(--brand-color-text-default)'}} />
 }
--- packages/theme/components/layout/article/Article.tsx
+++ packages/theme/components/layout/article/Article.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {PropsWithChildren} from 'react'
 import {TableOfContents} from '../table-of-contents/TableOfContents'
 import styles from './Article.module.css'
 import bodyStyles from '../../../css/prose.module.css'
+import {Heading as HeadingType} from 'nextra'
 
 import clsx from 'clsx'
 
-export function Article({children, toc, metadata}) {
+type Props = {
+  toc: HeadingType[]
+  metadata: {
+    layout?: string
+  }
+}
+
+export function Article({children, toc, metadata}: PropsWithChildren<Props>) {
   const hasToc = toc.length > 0
   return (
     <div className={clsx(styles.Article, hasToc && styles['Article--withToc'])}>
