695b557ee612fec7e51f05d557f209a30144672d
Josh Farrant <joshfarrant@github.com>
2025-04-16 09:35:52 +0100

    show links marked with 'page' in Header dropdown

--- packages/theme/components/layout/header/Header.tsx
+++ packages/theme/components/layout/header/Header.tsx
@@ -10,9 +10,12 @@ import styles from './Header.module.css'
 import {NavDrawer} from '../nav-drawer/NavDrawer'
 import {useNavDrawerState} from '../nav-drawer/useNavDrawerState'
 import {useColorMode} from '../../context/color-modes/useColorMode'
-import {DocsItem} from '../../../types'
+
+import {DocsItem, ExtendedPageItem} from '../../../types'
+
 import {GlobalSearch} from '../global-search/GlobalSearch'
 import {FocusOn} from 'react-focus-on'
+import {LinksDropdown} from '../links-dropdown/LinksDropdown'
 
 type HeaderProps = {
   pageMap: PageMapItem[]
@@ -26,6 +29,8 @@ export function Header({pageMap, siteTitle, flatDocsDirectories}: HeaderProps) {
   const searchTriggerRef = useRef<HTMLButtonElement | null>(null)
   const [isNavDrawerOpen, setIsNavDrawerOpen] = useNavDrawerState('768')
   const [isSearchOpen, setIsSearchOpen] = useState(false)
+
+  const headerExternalLinks = (pageMap as ExtendedPageItem[]).filter(page => page.type === 'page')
 
   useEffect(() => {
     document.documentElement.setAttribute('data-color-mode', colorMode)
@@ -44,57 +49,72 @@ export function Header({pageMap, siteTitle, flatDocsDirectories}: HeaderProps) {
       role="navigation"
       aria-label="Header Navigation"
     >
-      <Link href="/" className={styles.Header__siteTitle}>
-        <MarkGithubIcon size={24} />
-        <Text as="p" size="300" weight="semibold">
-          {siteTitle}
+      <div className={styles.Header__start}>
+        <Link href="/" className={styles.Header__siteTitle}>
+          <MarkGithubIcon size={24} />
+          <Text as="p" size="300" weight="semibold">
+            {siteTitle}
+          </Text>
+        </Link>
+        <Text as="span" className={styles.Header__separator} weight="semibold" aria-hidden>
+          &#47;
         </Text>
-      </Link>
-      <div className={clsx(styles.Header__searchArea, isSearchOpen && styles['Header__searchArea--open'])}>
-        <FocusOn enabled={isSearchOpen} onEscapeKey={closeSearch} onClickOutside={closeSearch}>
-          <GlobalSearch
-            ref={searchRef}
-            siteTitle={siteTitle}
-            flatDocsDirectories={flatDocsDirectories}
-            onNavigate={() => closeSearch()}
-          />
-          <div className={styles.Header__searchHeaderBanner}>
-            <Stack direction="horizontal" padding="none" gap={4} alignItems="center" justifyContent="space-between">
-              <Text as="p" size="300" weight="semibold">
-                Search
-              </Text>
-              <IconButton icon={XIcon} variant="invisible" aria-label="Close search" onClick={closeSearch} />
-            </Stack>
-          </div>
-        </FocusOn>
+        <LinksDropdown className={styles.Header__linksDropdown} items={headerExternalLinks} />
       </div>
-      <div>
-        <Stack direction="horizontal" padding="none" gap={4}>
-          <IconButton
-            icon={colorMode === 'light' ? SunIcon : MoonIcon}
-            variant="invisible"
-            aria-label={`Change color mode. Active mode is ${colorMode}.`}
-            onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
-          />
-          <IconButton
-            ref={searchTriggerRef}
-            className={styles.Header__searchButton}
-            icon={SearchIcon}
-            variant="invisible"
-            aria-label={`Open search`}
-            onClick={() => setIsSearchOpen(true)}
-          />
-          <div className={styles.Header__navDrawerContainer}>
-            <IconButton
-              icon={ThreeBarsIcon}
-              variant="invisible"
-              aria-label="Menu"
-              aria-expanded={isNavDrawerOpen}
-              onClick={() => setIsNavDrawerOpen(true)}
-            />
-            <NavDrawer isOpen={isNavDrawerOpen} onDismiss={() => setIsNavDrawerOpen(false)} navItems={pageMap} />
-          </div>
+      <div className={styles.Header__end}>
+        <Stack className={styles.Header__links} direction="horizontal" padding="none" gap={24}>
+          {headerExternalLinks.map(item => (
+            <Link key={item.href} className={styles.Header__link} href={item.href}>
+              <Text size="200">{item.title}</Text>
+            </Link>
+          ))}
         </Stack>
+        <div className={clsx(styles.Header__searchArea, isSearchOpen && styles['Header__searchArea--open'])}>
+          <FocusOn enabled={isSearchOpen} onEscapeKey={closeSearch} onClickOutside={closeSearch}>
+            <GlobalSearch
+              ref={searchRef}
+              siteTitle={siteTitle}
+              flatDocsDirectories={flatDocsDirectories}
+              onNavigate={() => closeSearch()}
+            />
+            <div className={styles.Header__searchHeaderBanner}>
+              <Stack direction="horizontal" padding="none" gap={4} alignItems="center" justifyContent="space-between">
+                <Text as="p" size="300" weight="semibold">
+                  Search
+                </Text>
+                <IconButton icon={XIcon} variant="invisible" aria-label="Close search" onClick={closeSearch} />
+              </Stack>
+            </div>
+          </FocusOn>
+        </div>
+        <div>
+          <Stack direction="horizontal" padding="none" gap={4}>
+            <IconButton
+              icon={colorMode === 'light' ? SunIcon : MoonIcon}
+              variant="invisible"
+              aria-label={`Change color mode. Active mode is ${colorMode}.`}
+              onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+            />
+            <IconButton
+              ref={searchTriggerRef}
+              className={styles.Header__searchButton}
+              icon={SearchIcon}
+              variant="invisible"
+              aria-label={`Open search`}
+              onClick={() => setIsSearchOpen(true)}
+            />
+            <div className={styles.Header__navDrawerContainer}>
+              <IconButton
+                icon={ThreeBarsIcon}
+                variant="invisible"
+                aria-label="Menu"
+                aria-expanded={isNavDrawerOpen}
+                onClick={() => setIsNavDrawerOpen(true)}
+              />
+              <NavDrawer isOpen={isNavDrawerOpen} onDismiss={() => setIsNavDrawerOpen(false)} navItems={pageMap} />
+            </div>
+          </Stack>
+        </div>
       </div>
     </nav>
   )
