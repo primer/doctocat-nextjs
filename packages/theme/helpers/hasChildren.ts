f2cc1337c9aa36ec9ab61871a1e33af225ffb6c1
Josh Farrant <joshfarrant@github.com>
2025-04-30 14:15:26 +0100

    remove unnecessary change

--- packages/theme/helpers/hasChildren.ts
+++ packages/theme/helpers/hasChildren.ts
@@ -1,4 +1,4 @@
-import type {PageMapItem, Folder} from 'nextra'
+import {PageMapItem, Folder} from 'nextra'
 
 export function hasChildren(item: PageMapItem): item is Folder {
   return 'children' in item && Array.isArray(item.children)
